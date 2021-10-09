a8e02e024f3e812aeb6ed63e9062b5f778545193
golden-state0fmind <areyes2009@yahoo.com>
2021-10-09 13:28:21 -0700

    adds REST API routes for full CRUD(create, read, update, destroy) functionality

---
+++ routes/user.js
@@ -0,0 +1,11 @@
+//imports
+const router = require('express').Router();
+const ctrl = require('../controllers');
+//routes
+router.get('/', ctrl.user.index);
+router.get('/:id', ctrl.user.getOne);
+router.post('/', ctrl.user.create);
+router.put('/:id', ctrl.user.update);
+router.delete('/:id', ctrl.user.destroy);
+//exports
+module.exports = router;
